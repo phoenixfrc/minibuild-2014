dafceccd487fd68671b5d2d013e639505b26ba06
team2342 <software@team2342.org>
2014-11-08 15:48:07 -0500

    forgot PortAssignments.h from last commit

---
+++ PortAssignments.h
@@ -0,0 +1,9 @@
+struct PortAssign
+{
+	//Motor Ports
+	static const int leftMotorChannel = 1;
+	static const int rightMotorChannel = 2;
+	//Joystick Ports
+	static const int leftJoystickID  = 1;
+	static const int rightJoystickID = 2;
+};
