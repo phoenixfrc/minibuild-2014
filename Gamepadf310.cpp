4f139c4937cbd44587d9d5f5ff4418d2f134c19c
team2342 <controls@team2342.org>
2014-11-10 19:41:22 -0500

    added Classes that derive from Joystick for the gamepad.

---
+++ Gamepadf310.cpp
@@ -0,0 +1,10 @@
+#include "Gamepadf310.h"
+
+Gamepadf310::Gamepadf310(uint32_t port):
+Joystick(port){
+}
+Gamepadf310::Gamepadf310(uint32_t port, uint32_t numAxisTypes, uint32_t numButtonTypes):
+	Joystick(port,numAxisTypes,numButtonTypes){
+}
+ Gamepadf310::~Gamepadf310(){
+ }
---
+++ Gamepadf310.h
@@ -0,0 +1,11 @@
+#ifndef GAMEPADF310_H
+#define GAMEPADF310_H
+#include "WPILib.h"
+class Gamepadf310 : public Joystick
+
+{
+	explicit Gamepadf310(uint32_t port);
+	Gamepadf310(uint32_t port, uint32_t numAxisTypes, uint32_t numButtonTypes);
+	virtual ~Gamepadf310();
+};
+#endif
