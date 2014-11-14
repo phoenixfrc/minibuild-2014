79654d6a47b0d3ec10511c9599e01bc1fcb5077c
team2342 <software@team2342.org>
2014-11-13 20:48:55 -0500

    Made some constants for the buttons

--- Gamepadf310.h
+++ Gamepadf310.h
@@ -6,5 +6,9 @@ class Gamepadf310 : public Joystick
 	explicit Gamepadf310(uint32_t port);
 	Gamepadf310(uint32_t port, uint32_t numAxisTypes, uint32_t numButtonTypes);
 	virtual ~Gamepadf310();
+	static const int X = 1;
+	static const int Y = 4;
+	static const int A = 2;
+	static const int B = 3;
 };
 #endif
