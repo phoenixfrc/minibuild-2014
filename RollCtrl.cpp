f4a0f93b1bd2c18b3e74d14af4594c417c1d9426
6x10tothe23rd <krdgenius@gmail.com>
2014-11-20 19:03:31 -0500

    Cleaned up roller code (made more efficient). Fixed roller code to respond to input actively rather than just roll for a set amount of time.

--- RollCtrl.cpp
+++ RollCtrl.cpp
@@ -21,61 +21,56 @@ void RollCtrl::performRollerTasks()
 
     //Getting these ready, these viariables are only true during the first
     //iteration when a button is pressed (this way we only perform an action once)
-    bool X_Pressed = X_Down;
-    bool Y_Pressed = Y_Down;
-    bool A_Pressed = A_Down;
-    bool B_Pressed = B_Down;
+    bool X_Pressed = false;
+    bool Y_Pressed = false;
+    bool A_Pressed = false;
+    bool B_Pressed = false;
 
     //Tests to set the value of these variables
-    if(!m_pressedALast && A_Down)
+    if(A_Down)
     {
-        A_Pressed = true;
+    	A_Pressed = !m_pressedALast;
     }
-    if(m_pressedALast && A_Down)
+    if(B_Down)
     {
-        A_Pressed = false;
+    	B_Pressed = !m_pressedBLast;
     }
-    if(!m_pressedBLast && B_Down)
+    if(X_Down)
     {
-        B_Pressed = true;
+    	X_Pressed = !m_pressedXLast;
     }
-    if(m_pressedBLast && B_Down)
+    if(Y_Down)
     {
-        B_Pressed = false;
-    }
-    if(!m_pressedXLast && X_Down)
-    {
-        X_Pressed = true;
-    }
-    if(m_pressedXLast && X_Down)
-    {
-        X_Pressed = false;
-    }
-    if(!m_pressedYLast && Y_Down)
-    {
-        Y_Pressed = true;
-    }
-    if(m_pressedYLast && Y_Down)
-    {
-        Y_Pressed = false;
+    	Y_Pressed = !m_pressedYLast;
     }
 
     //Assumptions we're making:
     //A Controls out rolling
     //B Controls in rolling
     //This can be changed later based on mech ideas
-    if (A_Down) {
-        m_roller.Set(outRollSpeed);
-        m_remainingRollTimeSec = maxRollTimeSec;
-    } else if (B_Down) {
-        m_roller.Set(inRollSpeed);
-        m_remainingRollTimeSec = maxRollTimeSec;
-    } else if (m_remainingRollTimeSec > 0.0) {
-        m_remainingRollTimeSec -= RobotConstants::teleopPauseDelaySec;
-    } else {
-        m_roller.StopMotor();
+    if(A_Pressed)
+    {
+    	if(m_desiredState == forward)
+    	{
+            m_desiredState = off;
+    	}
+    	else
+    	{
+    		m_desiredState = forward;
+    	}
     }
-
+    if(B_Pressed)
+    {
+    	if(m_desiredState == backward)
+    	{
+            m_desiredState = off;
+    	}
+    	else
+    	{
+    		m_desiredState = backward;
+    	}
+    }
+    
     //Setting up variables for next iteration
     m_pressedALast = A_Down;
     m_pressedBLast = B_Down;
