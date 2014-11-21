fde35e5162bcb8cf3edaca372fd23d15b7b03d2a
Benjamin Nichols-Farquhar <erogone775@gmail.com>
2014-11-20 20:57:17 -0500

    Added Camera Control Function

--- CameraControl.cpp
+++ CameraControl.cpp
@@ -7,3 +7,25 @@ OurCamera::OurCamera(Servo * pivotMotor, Servo * elevateMotor)
     m_cameraElevateAngle = 0;
     m_cameraPivotAngle = 0;
 }
+
+void OurCamera::CameraUpdate()
+{
+    if (gamePad.GetY() > 0.05 ||
+            gamePad.GetY() < -0.05) {
+        cameraElevateAngle += gamePad.GetY();
+        if (cameraElevateAngle < cameraElevateMotor.GetMinAngle())
+            cameraElevateAngle = cameraElevateMotor.GetMinAngle();
+        if (cameraElevateAngle > cameraElevateMotor.GetMaxAngle())
+            cameraElevateAngle = cameraElevateMotor.GetMaxAngle();
+    }
+    if (gamePad.GetX() > 0.05 ||
+            gamePad.GetX() < -0.05) {
+        cameraPivotAngle += gamePad.GetX();
+        if (cameraPivotAngle < cameraPivotMotor.GetMinAngle())
+            cameraPivotAngle = cameraPivotMotor.GetMinAngle();
+        if (cameraPivotAngle > cameraPivotMotor.GetMaxAngle())
+            cameraPivotAngle = cameraPivotMotor.GetMaxAngle();
+    }
+    cameraPivotMotor.SetAngle(cameraPivotAngle);
+    cameraElevateMotor.SetAngle(cameraElevateAngle);
+}
