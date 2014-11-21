123adf8647434c9b6de4724713d309002bead5d2
Will Gebhardt <Willd1999@gmail.com>
2014-11-20 20:34:06 -0500

    RollCtrl Changes
    Wrote a function to slope the change in motor speed

--- RollCtrl.cpp
+++ RollCtrl.cpp
@@ -9,6 +9,7 @@ m_roller(talonPort), m_remainingRollTimeSec(0.0)
     m_pressedBLast = false;
     m_pressedXLast = false;
     m_pressedYLast = false;
+    m_currentSpeed = 0;
 }
 
 void RollCtrl::performRollerTasks()
@@ -81,4 +82,52 @@ void RollCtrl::performRollerTasks()
     m_pressedBLast = B_Down;
     m_pressedXLast = X_Down;
     m_pressedYLast = Y_Down;
+}
+
+void RollCtrl::updateState(rollStates dState)
+{
+    // off code
+    if(dState == off)
+    {
+        if (m_currentSpeed < -interval)
+        {
+            m_currentSpeed += interval;
+        }
+        else if (m_currentSpeed > interval)
+        {
+            m_currentSpeed -= interval;
+        }
+        else
+        {
+            m_currentSpeed = 0;
+        }
+    }
+
+    // forward code
+    if(dState == forward)
+    {
+        if (m_currentSpeed < inRollSpeed - interval)
+        {
+            m_currentSpeed += interval;
+        }
+        else
+        {
+            m_currentSpeed = inRollSpeed;
+        }
+    }
+
+    // backward code
+
+    if(dState == backward)
+    {
+        if (m_currentSpeed > outRollSpeed + interval)
+        {
+            m_currentSpeed -= interval;
+        }
+        else
+        {
+            m_currentSpeed = outRollSpeed;
+        }
+    }
+    m_roller.set(m_currentSpeed);
 }
