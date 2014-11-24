f15387961dcac94e1ef714cc8de57294fafe0db0
Benjamin Nichols-Farquhar <erogone775@gmail.com>
2014-11-24 18:37:09 -0500

    Continued work on camera

--- CameraControl.cpp
+++ CameraControl.cpp
@@ -10,22 +10,4 @@ OurCamera::OurCamera(Servo * pivotMotor, Servo * elevateMotor)
 
 void OurCamera::CameraUpdate()
 {
-    if (gamePad.GetY() > 0.05 ||
-            gamePad.GetY() < -0.05) {
-        cameraElevateAngle += gamePad.GetY();
-        if (cameraElevateAngle < cameraElevateMotor.GetMinAngle())
-            cameraElevateAngle = cameraElevateMotor.GetMinAngle();
-        if (cameraElevateAngle > cameraElevateMotor.GetMaxAngle())
-            cameraElevateAngle = cameraElevateMotor.GetMaxAngle();
-    }
-    if (gamePad.GetX() > 0.05 ||
-            gamePad.GetX() < -0.05) {
-        cameraPivotAngle += gamePad.GetX();
-        if (cameraPivotAngle < cameraPivotMotor.GetMinAngle())
-            cameraPivotAngle = cameraPivotMotor.GetMinAngle();
-        if (cameraPivotAngle > cameraPivotMotor.GetMaxAngle())
-            cameraPivotAngle = cameraPivotMotor.GetMaxAngle();
-    }
-    cameraPivotMotor.SetAngle(cameraPivotAngle);
-    cameraElevateMotor.SetAngle(cameraElevateAngle);
 }
